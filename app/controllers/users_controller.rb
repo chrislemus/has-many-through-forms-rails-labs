be2def49dce0f1e0c5ef34fcf1cefb24ba5b3d8a
chrislemus <icrislemus@gmail.com>
2021-02-24 19:54:24 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @commented_posts = @user.comments.map{|c| c.post}.uniq
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  # accepts_nested_attributes_for :user_attributes
+
+  def user_attributes=(attr)
+    username = attr[:username]
+    if username.length > 0
+      user = User.find_or_create_by(username: username)
+      self.user = user
+    end
+  end
 
 end
