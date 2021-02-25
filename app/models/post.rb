fb5b738900a8b2a2ef0cd910658818fbe9b52bf6
chrislemus <icrislemus@gmail.com>
2021-02-24 19:54:16 -0500

    commit tree bb911fd08c9d2295a4c2154e812ddb0b507264ba

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,21 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories_attributes
+
+  # def comment(comment)
+  #   comment
+  # end
+
+  def categories_attributes=(attrs)
+    attrs.each do |attr|
+      category_name = attr['name']
+      unless category_name.empty?
+        category = Category.find_or_create_by(name: category_name)
+        self.categories << category
+      end
+    end
+  end
 
 
 end
